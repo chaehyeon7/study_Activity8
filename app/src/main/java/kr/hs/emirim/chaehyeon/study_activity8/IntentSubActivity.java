6f0aa0975405b1d37d161ef1835d258f2f8bdad0
chaehyeon7 <81363193+chaehyeon7@users.noreply.github.com>
2021-08-20 18:28:21 +0900

    Edit Add Intent (IntentSubActivity.java) Commit

---
+++ app/src/main/java/kr/hs/emirim/chaehyeon/study_activity8/IntentSubActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.chaehyeon.study_activity8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntentSubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_sub);
+    }
+}
--- app/src/main/java/kr/hs/emirim/chaehyeon/study_activity8/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/chaehyeon/study_activity8/MainActivity.java
@@ -2,13 +2,31 @@ package kr.hs.emirim.chaehyeon.study_activity8;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button next_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        next_btn = findViewById(R.id.next_btn);
+        next_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //ntentSubActivity로 화면 전환
+                Intent i = new Intent( MainActivity.this, IntentSubActivity.class);
+
+                startActivity( i );  //지정해 놓은 페이지로 화면을 전환
+            }
+        });
+
     }
 }
